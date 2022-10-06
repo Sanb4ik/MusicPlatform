23538848dbacafdb89e1b1244cae5a536b5ec3b0
Sanb4ik <sashabelaz7@gmail.com>
2022-10-06 22:45:25 +0300

    fethTrak func

--- client/store/action-creators/track.ts
+++ client/store/action-creators/track.ts
@@ -16,3 +16,18 @@ export const fetchTracks = async() => {
         }
     }
 }
+
+export const fetchTrack = async(url: string) => {
+    return async (dispatch: Dispatch<TrackAction>) =>{
+        try {
+            const response = await axios.get('http://localhost:3333/tracks'+url)
+             console.log(response);
+            dispatch({type: TrackActionTypes.FETCH_TRACK, payload: response.data});
+        }catch (err){
+            dispatch({
+                type: TrackActionTypes.FETCH_TRACKS_ERROR,
+                payload: 'fetchTrack failed'
+            });
+        }
+    }
+}
